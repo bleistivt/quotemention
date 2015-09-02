0212fa39d5f2d63f5c3de65a0ad398287f1f0490
bleistivt <me@bleistivt.net>
2015-09-02 10:58:03 +0200

    still not it

--- js/quotemention.js
+++ js/quotemention.js
@@ -64,7 +64,7 @@ jQuery(function ($) {
         }
 
         // Show the tooltip if it is loading or if the post is not fully visible.
-        if (showProgress && !loaded || !inview(target)) {
+        if (!loaded && showProgress || loaded && !inview(target)) {
             mention.tooltipster('show');
         }
     }
